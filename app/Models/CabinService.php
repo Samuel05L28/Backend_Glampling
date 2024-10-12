a3c615d21b90eeb7e58fa853e5236ade67c15001
ArenasAg <arenasanderson63@gmail.com>
2024-10-12 02:07:54 -0500

    Refactor models to include new fields and relationships

--- app/Models/CabinService.php
+++ app/Models/CabinService.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reservation extends Model
+class CabinService extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'cabins_id',
-        'users_id',
+        'service_id'
     ];
 }
